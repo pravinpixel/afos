12af3af4bba54cd3659f7c09151e9e541a148d32
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-07-14 15:06:55 +0530

    Slogan changes

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -28,5 +28,5 @@
 </nav>
 <div class="text-center mt-4" style="font-size: 20px;">
     <span class="text-secondary">“ Let food be thy medicine, let medicine be thy food ” </span> 
-    <br> by <span class="text-dark"><b>Hippocrates</b></span>.
+    <br> - by <span class="text-dark"><b>Hippocrates</b></span>.
 </div>
